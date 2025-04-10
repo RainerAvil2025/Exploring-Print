f8c6e728af377a384c3723d1e8c7453541df4eff
RainerAvil2025 <raineravil3@gmail.com>
2025-04-10 16:25:30 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+#print a sentence
+print("Welcome to the world of programming")
+#print the word
+print("Welcome")
+#print a number
+print(4)
+#next line after the print
+print("Hello Python,\n")
+#pass the multiple arguments to the print statement
+print("Hello",5)
+#end arguement of print statement
+print("Welcome to",end="*")
