1716691cd4ee6f1ab602be0d25f2850f9f18b56e
RainerAvil2025 <raineravil3@gmail.com>
2025-04-15 16:45:05 +0530

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,21 @@
+print("Enter a name")
+name=input("Enter a name")
+print("\nHello",name,"Welcome to codingal")
+
+f=10.10
+i=int(f)
+print(i)
+
+name="penguin"
+age=15
+is_student=True
+weight=38.5
+#printing different variables and the data types
+print("name",name)
+print("data type of name is",type(name))
+print("age",age)
+print("data type of age is",type(age))
+print("is_student",is_student)
+print("data type of is_student",type(is_student))
+print("weight",weight)
+print("data type of weight is",type(weight))
---
+++ stringoperation.py
@@ -0,0 +1,8 @@
+#input a word
+text=str(input("Enter a string"))
+#reverse string
+#using step value as-1
+revtext=text[::-1]
+text=revtext
+print("reverse of given string is")
+print(text)
