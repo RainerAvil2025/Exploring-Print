78b9176c3dc8df1b221afe60cf4c7eb0324e309e
RainerAvil2025 <raineravil3@gmail.com>
2025-04-24 16:25:41 +0530

    Add files via upload

---
+++ and-or.py
@@ -0,0 +1,16 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print("All numbers have boolian value as true")
+else:
+    print("At least one number has the boolian value as false")
+
+    a=10
+    b=-10
+    c=10
+
+    if a>0 or b>0:
+        print("Either of the number is greater than 0")
+    else:
+        print("No number is greater than 0")
